30fc1274d06b3b1fd95856c901032a34a9eb8162
Joey Bloom <joey.bloom@acxiom.com>
2018-12-05 16:20:38 -0600

    Added new Java code for testing

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,11 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+		for (int j = 0; j < 50; j++) {
+					// Add header and value to temp row
+			System.out.print("Joey is cool");
+
+		}//for
     }
 
     private final String getMessage() {
