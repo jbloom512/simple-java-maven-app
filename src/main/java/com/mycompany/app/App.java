ef2f661e2e917ca51c1d33c4f9d56c956afb8b28
Joey Bloom <joey.bloom@acxiom.com>
2018-12-05 16:28:35 -0600

    Correct code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-		for (int j = 0; j < header.length; j++) {
+		for (int j = 0; j < 50; j++) {
 			System.out.println("This is a test");
 
 		}//for
